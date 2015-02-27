6836420c3c61eede29ad0a64cb137d9a100cf426
Jason Mehring <nrgaway@gmail.com>
2015-02-27 00:52:17 -0500

    Removed nautilus-actions depend and replaced with nautilus-python
    nautilus-actions was orphaned in fc21, so all nautilus context menus have
    been re-written as nautilus-python extensions

---
+++ qubes-rpc/qvm_move_nautilus.py
@@ -0,0 +1,36 @@
+import subprocess
+
+from gi.repository import Nautilus, GObject
+
+
+class MoveToAppvmItemExtension(GObject.GObject, Nautilus.MenuProvider):
+    '''Move file(s) to AppVM.
+
+    Uses the nautilus-python api to provide a context menu within Nautilus which
+    will enable the user to select file(s) to to move to another AppVM
+    '''
+    def get_file_items(self, window, files):
+        '''Attaches context menu in Nautilus
+        '''
+        if not files:
+            return
+
+        menu_item = Nautilus.MenuItem(name='QubesMenuProvider::MoveToAppvm',
+                                      label='Move To Other AppVM...',
+                                      tip='',
+                                      icon='')
+
+        menu_item.connect('activate', self.on_menu_item_clicked, files)
+        return menu_item,
+
+    def on_menu_item_clicked(self, menu, files):
+        '''Called when user chooses files though Nautilus context menu.
+        '''
+        for file_obj in files:
+
+            # Check if file still exists
+            if file_obj.is_gone():
+                return
+
+            gio_file = file_obj.get_location()
+            subprocess.call(['/usr/lib/qubes/qvm-move-to-vm.gnome', gio_file.get_path()])
