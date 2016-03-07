1fabb678e265f213ebf4656b5dd431f0a041942e
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2016-03-07 13:37:45 +0100

    network: use `qubes-primary-dns` QubesDB entry if present
    For a long time the DNS address was the same as default gateway. This is
    still the case in R3.x, but using `qubes-gateway` configuration
    parameter for it is misleading. It should be up to dom0 to provide DNS
    address (whether the value is the same as gateway or not).
    
    Fixes QubesOS/qubes-issues#1817
    
    (cherry picked from commit fb9b3b62c02427b80657056805a9bac7756c98b3)

--- vm-systemd/network-proxy-setup.sh
+++ vm-systemd/network-proxy-setup.sh
@@ -5,9 +5,15 @@ network=$(qubesdb-read /qubes-netvm-network 2>/dev/null)
 if [ "x$network" != "x" ]; then
     gateway=$(qubesdb-read /qubes-netvm-gateway)
     netmask=$(qubesdb-read /qubes-netvm-netmask)
+    primary_dns=$(qubesdb-read /qubes-netvm-primary-dns 2>/dev/null || echo $gateway)
     secondary_dns=$(qubesdb-read /qubes-netvm-secondary-dns)
+<<<<<<< HEAD
     modprobe netbk 2> /dev/null || modprobe xen-netback
     echo "NS1=$gateway" > /var/run/qubes/qubes-ns
+=======
+    modprobe netbk 2> /dev/null || modprobe xen-netback || "${modprobe_fail_cmd}"
+    echo "NS1=$primary_dns" > /var/run/qubes/qubes-ns
+>>>>>>> fb9b3b6... network: use `qubes-primary-dns` QubesDB entry if present
     echo "NS2=$secondary_dns" >> /var/run/qubes/qubes-ns
     /usr/lib/qubes/qubes-setup-dnat-to-ns
     echo "1" > /proc/sys/net/ipv4/ip_forward
