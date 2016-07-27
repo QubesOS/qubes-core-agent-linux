a4b53f557acdb0b1d3a043a5c9feffc4e7f377d7
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2016-07-28 00:15:39 +0200

    qvm-open-in-vm: fix wrapping URL in HTML file
    The first line of HTML wasn't written to the file.
    
    Fixes QubesOS/qubes-issues#2205

--- qubes-rpc/wrap-in-html-if-url.sh
+++ qubes-rpc/wrap-in-html-if-url.sh
@@ -6,8 +6,8 @@ wrap_in_html_if_url()
 	*://*)
 		FILE_ARGUMENT=$(mktemp)
 
-		echo -n '<html><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>'
-        echo -n '<meta HTTP-EQUIV="REFRESH" content="0; url=' > $FILE_ARGUMENT
+		echo -n '<html><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>' > $FILE_ARGUMENT
+		echo -n '<meta HTTP-EQUIV="REFRESH" content="0; url=' >> $FILE_ARGUMENT
 		echo -n "$1" | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g' >> $FILE_ARGUMENT
 		echo '"/></html>' >> $FILE_ARGUMENT
 		;;
