b16508a0937919a6ef85afb0c57b3be1473bc520
Rusty Bird <rustybird@openmailbox.org>
2016-12-13 14:25:32 +0000

    Set $QREXEC_REMOTE_DOMAIN_ID to Xen domid

--- qrexec/qrexec-agent-data.c
+++ qrexec/qrexec-agent-data.c
@@ -57,13 +57,16 @@ static void sigusr1_handler(int __attribute__((__unused__))x)
     signal(SIGUSR1, SIG_IGN);
 }
 
-void prepare_child_env() {
+void prepare_child_env(int remote_domid) {
     char pid_s[10];
+    char remote_domid_s[16];
 
     signal(SIGCHLD, sigchld_handler);
     signal(SIGUSR1, sigusr1_handler);
     snprintf(pid_s, sizeof(pid_s), "%d", getpid());
     setenv("QREXEC_AGENT_PID", pid_s, 1);
+    snprintf(remote_domid_s, sizeof(remote_domid_s), "%d", remote_domid);
+    setenv("QREXEC_REMOTE_DOMAIN_ID", remote_domid_s, 1);
 }
 
 int handle_handshake(libvchan_t *ctrl)
@@ -511,7 +514,7 @@ int handle_new_process_common(int type, int connect_domain, int connect_port,
     }
     handle_handshake(data_vchan);
 
-    prepare_child_env();
+    prepare_child_env(connect_domain);
     /* TODO: use setresuid to allow child process to actually send the signal? */
 
     switch (type) {
