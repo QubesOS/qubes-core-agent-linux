45f06a7863fe6f3d1b19eb6a47603654979cd653
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-07-16 11:09:14 +0200

    Announce if qubes-firewall service is supported+enabled in this template
    Fixes QubesOS/qubes-issues#2003

--- post-install.d/10-qubes-core-agent-features.sh
+++ post-install.d/10-qubes-core-agent-features.sh
@@ -7,3 +7,9 @@ qvm-features-request qrexec=1
 if [ -x /usr/bin/qubes-gui ]; then
     qvm-features-request gui=1
 fi
+
+if systemctl -q is-enabled qubes-firewall.service 2>/dev/null; then
+    qvm-features-request qubes-firewall=1
+else
+    qvm-features-request qubes-firewall=0
+fi
