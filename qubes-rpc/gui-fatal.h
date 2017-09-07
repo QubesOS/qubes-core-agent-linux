0f56bc904e0be855ea9631f242264ef13c8943dd
Frederic Pierret (Epitre) <frederic.epitre@orange.fr>
2017-09-07 15:22:31 +0200

    Handle fallthrough with attribute(noreturn) for consistancy and compatiblity with older GCC
    (cherry picked from commit 11f86ca4e43ce1c4abfa1151b8d23c8b559c9cf3)

--- qubes-rpc/gui-fatal.h
+++ qubes-rpc/gui-fatal.h
@@ -1,3 +1,3 @@
-void gui_fatal(const char *fmt, ...);
+void gui_fatal(const char *fmt, ...) __attribute__((noreturn));
 void gui_nonfatal(const char *fmt, ...);
-void qfile_gui_fatal(const char *fmt, va_list args);
+void qfile_gui_fatal(const char *fmt, va_list args) __attribute__((noreturn));
