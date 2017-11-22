c452cca812f5d92803ae0480c933738dda6e06c0
Frédéric Pierret <frederic.epitre@orange.fr>
2017-11-22 13:31:33 +0100

    Fix ShellCheck comments

--- qubes-rpc/qvm-actions.sh
+++ qubes-rpc/qvm-actions.sh
@@ -15,33 +15,36 @@ files=${*:2}
 # copy and move handle a list of files where other actions don't
 case $1 in
     copy)
-        qvm-copy-to-vm '$default' $files | zenity --notification --text="Copying files..." --timeout 3
+        #shellcheck disable=SC2016
+        qvm-copy-to-vm '$default' "$files" | zenity --notification --text="Copying files..." --timeout 3
         ;;
     move)
-        qvm-move-to-vm '$default' $files | zenity --notification --text="Moving files..." --timeout 3
+        #shellcheck disable=SC2016
+        qvm-move-to-vm '$default' "$files" | zenity --notification --text="Moving files..." --timeout 3
         ;;
     img)
         for file in $files
         do
-            /usr/lib/qubes/qvm-convert-img.gnome $file
+            /usr/lib/qubes/qvm-convert-img.gnome "$file"
         done
         ;;
     pdf)
         for file in $files
         do
-            /usr/lib/qubes/qvm-convert-pdf.gnome $file
+            /usr/lib/qubes/qvm-convert-pdf.gnome "$file"
         done
         ;;
     openvm)
         for file in $files
         do
-            qvm-open-in-vm '$default' $file | zenity --notification --text "Opening $file in VM..." --timeout 3 &
+            #shellcheck disable=SC2016
+            qvm-open-in-vm '$default' "$file" | zenity --notification --text "Opening $file in VM..." --timeout 3 &
         done
         ;;
     opendvm)
         for file in $files
         do
-            qvm-open-in-dvm $files | zenity --notification --text "Opening $file in DisposableVM..." --timeout 3 &
+            qvm-open-in-dvm "$files" | zenity --notification --text "Opening $file in DisposableVM..." --timeout 3 &
         done
         ;;
     *)
