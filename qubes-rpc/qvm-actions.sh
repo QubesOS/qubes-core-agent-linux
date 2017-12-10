c34a0a9e074da62c47490892b0784b554f7333fb
Frédéric Pierret <frederic.epitre@orange.fr>
2017-12-10 11:19:34 +0100

    Fix UCA mistake and qvm-actions script

--- qubes-rpc/qvm-actions.sh
+++ qubes-rpc/qvm-actions.sh
@@ -1,50 +1,48 @@
 #!/bin/bash
 
-# Allow us to handle filenames with spaces using newline separator from Thunar
-IFS='
-'
-
 # Check if at least two arguments are provided: actions + file(s)
 if [ "$#" -le 1 ]; then
     echo "Not enough arguments provided. Aborting..."
 fi
 
-# File(s)
-files=${*:2}
+# Action
+action="$1"
+
+shift
 
 # copy and move handle a list of files where other actions don't
-case $1 in
+case "$action" in
     copy)
         #shellcheck disable=SC2016
-        qvm-copy-to-vm '$default' "$files" | zenity --notification --text="Copying files..." --timeout 3
+        qvm-copy-to-vm '$default' "$@" | zenity --notification --text="Copying files..." --timeout 3
         ;;
     move)
         #shellcheck disable=SC2016
-        qvm-move-to-vm '$default' "$files" | zenity --notification --text="Moving files..." --timeout 3
+        qvm-move-to-vm '$default' "$@" | zenity --notification --text="Moving files..." --timeout 3
         ;;
     img)
-        for file in $files
+        for file in "$@"
         do
             /usr/lib/qubes/qvm-convert-img.gnome "$file"
         done
         ;;
     pdf)
-        for file in $files
+        for file in "$@"
         do
             /usr/lib/qubes/qvm-convert-pdf.gnome "$file"
         done
         ;;
     openvm)
-        for file in $files
+        for file in "$@"
         do
             #shellcheck disable=SC2016
             qvm-open-in-vm '$default' "$file" | zenity --notification --text "Opening $file in VM..." --timeout 3 &
         done
         ;;
     opendvm)
-        for file in $files
+        for file in "$@"
         do
-            qvm-open-in-dvm "$files" | zenity --notification --text "Opening $file in DisposableVM..." --timeout 3 &
+            qvm-open-in-dvm "$file" | zenity --notification --text "Opening $file in DisposableVM..." --timeout 3 &
         done
         ;;
     *)
