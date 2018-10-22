d285cbf3492b769db5467bed4a43b7ac51abc829
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-10-22 16:25:51 +0200

    Do not force /bin/sh for /etc/qubes-rpc/qubes.GetAppmenus
    Call it as normal executable instead of forcing it through /bin/sh.
    Especially when now it have bash shebang.
    
    Fixes QubesOS/qubes-issues#4417

--- misc/qubes-trigger-sync-appmenus.sh
+++ misc/qubes-trigger-sync-appmenus.sh
@@ -5,5 +5,5 @@
 . /usr/lib/qubes/init/functions
 
 if is_updateable ; then
-    /usr/lib/qubes/qrexec-client-vm dom0 qubes.SyncAppMenus /bin/sh /etc/qubes-rpc/qubes.GetAppmenus
+    /usr/lib/qubes/qrexec-client-vm dom0 qubes.SyncAppMenus /etc/qubes-rpc/qubes.GetAppmenus
 fi
