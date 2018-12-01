98a6b60a49ad1215918cc0d89faf4e32e1b69890
AJ Jordan <alex@strugee.net>
2018-11-30 23:47:35 -0500

    Remove unnecessary quotes

--- init/resize-rootfs-if-needed.sh
+++ init/resize-rootfs-if-needed.sh
@@ -16,7 +16,7 @@ sysfs_xvda="/sys/class/block/xvda"
 # if root filesystem use already (almost) the whole dis
 non_rootfs_data=$(( 250 * 1024 * 2 ))
 rootfs_size=$(df --block-size=512 --output=size / | tail -n 1)
-if [ "$(cat "$sysfs_xvda/size")" -lt \
+if [ $(cat $sysfs_xvda/size) -lt \
        $(( non_rootfs_data + rootfs_size )) ]; then
    echo "root filesystem already at $rootfs_size blocks" >&2
    exit 0
