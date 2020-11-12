dd8de797e3bb0f60bdfc293228beb384242af352
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2020-11-12 01:37:12 +0100

    Move network uplink setup to a separate service
    Previously, network uplink (eth0) was configured in two places:
     - udev (asynchronously)
     - qubes-misc-post.service - at the very end of the boot process
    
    This caused multiple issues:
    1. Depending on udev event processing (non-deterministic), network
       uplink could be enabled too early, for example before setting up
       firewall.
    2. Again depending on udev processing, it can be enabled quite late in
       the boot process, after network.target is up and services assume
       network already configured. This for example causes qubes-firewall to
       fail DNS queries.
    3. If udev happen try to enable enable networking even earlier, it may
       happend before qubesdb-daemon is started, in which case network setup
       fill fail. For this case, there was network re-setup in
       qubes-misc-post service - much later in the boot.
    
    Fix the above by placing network uplink setup in a dedicated
    qubes-network-uplink@${INTERFACE}.service unit ordered after
    network-pre.target and pulled in by udev based on vif device existence,
    to handle also dynamic network attach/detach.
    Then, create qubes-network-uplink.service unit waiting for appropriate
    interface-specific unit (if one is expected!) and order it before
    network.target.
    
    QubesOS/qubes-issues#5576

--- vm-systemd/misc-post.sh
+++ vm-systemd/misc-post.sh
@@ -11,15 +11,6 @@ if [ -n "$(ls -A /usr/local/lib 2>/dev/null)" ] || \
     ldconfig
 fi
 
-# Set IP address again (besides action in udev rules); this is needed by
-# DispVM (to override DispVM-template IP) and in case when qubes-ip was
-# called by udev before loading evtchn kernel module - in which case
-# qubesdb-read fails
-QUBES_MANAGED_IFACE="$(get_qubes_managed_iface)"
-if [ "x$QUBES_MANAGED_IFACE" != "x" ]; then
-INTERFACE="$QUBES_MANAGED_IFACE" ACTION="add" /usr/lib/qubes/setup-ip
-fi
-
 if [ -x /rw/config/rc.local ] ; then
     /rw/config/rc.local
 fi
