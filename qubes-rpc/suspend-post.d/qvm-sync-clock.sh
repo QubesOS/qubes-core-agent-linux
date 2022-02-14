5988b77d06adc1c73d2a18902f42f0145cebd7d4
Patrick Schleizer <adrelanos@whonix.org>
2022-02-14 05:08:06 -0500

    use common entry point for qvm-sync-clock
    fixes https://github.com/QubesOS/qubes-issues/issues/7265

--- qubes-rpc/suspend-post.d/qvm-sync-clock.sh
+++ qubes-rpc/suspend-post.d/qvm-sync-clock.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 if [ ! -f /var/run/qubes-service/clocksync ]; then
-    /usr/bin/qvm-sync-clock
+    # https://github.com/QubesOS/qubes-issues/issues/7265
+    systemctl --no-pager restart qubes-sync-time.service
 fi
