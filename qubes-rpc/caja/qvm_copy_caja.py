638b0f56c3474436a7915e525c66796c27c67635
makoshinra <makoshinra@protonmail.com>
2022-05-21 11:24:44 -0400

    qvm-copy: convert nautilus script to work with caja

--- qubes-rpc/caja/qvm_copy_caja.py
+++ qubes-rpc/caja/qvm_copy_caja.py
@@ -1,30 +1,30 @@
 import subprocess
 
-from gi.repository import Nautilus, GObject
+from gi.repository import Caja, GObject
 
 
-class CopyToAppvmItemExtension(GObject.GObject, Nautilus.MenuProvider):
+class CopyToAppvmItemExtension(GObject.GObject, Caja.MenuProvider):
     '''Copy file(s) to AppVM.
 
-    Uses the nautilus-python api to provide a context menu with Nautilus which
+    Uses the caja-python api to provide a context menu with Caja which
     will enable the user to select file(s) to to copy to another AppVM
     '''
     def get_file_items(self, window, files):
-        '''Attaches context menu in Nautilus
+        '''Attaches context menu in Caja
         '''
         if not files:
             return
 
-        menu_item = Nautilus.MenuItem(name='QubesMenuProvider::CopyToAppvm',
-                                      label='Copy To Other AppVM...',
-                                      tip='',
-                                      icon='')
+        menu_item = Caja.MenuItem(name='QubesMenuProvider::CopyToAppvm',
+                                  label='Copy To Other AppVM...',
+                                  tip='',
+                                  icon='')
 
         menu_item.connect('activate', self.on_menu_item_clicked, files)
         return menu_item,
 
     def on_menu_item_clicked(self, menu, files):
-        '''Called when user chooses files though Nautilus context menu.
+        '''Called when user chooses files though Caja context menu.
         '''
         cmd = [file_obj.get_location().get_path()
                for file_obj in files
