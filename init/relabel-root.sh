23e3624130e2e9d9ddb66af9fb97479fa9e7a19e
Demi Marie Obenour <demi@invisiblethingslab.com>
2022-11-23 01:49:35 -0500

    shut up shellcheck
    This file will always exist at runtime.

--- init/relabel-root.sh
+++ init/relabel-root.sh
@@ -1,5 +1,6 @@
 #!/bin/sh --
 set -eu
+# shellcheck disable=SC1091
 . /etc/selinux/config
 echo 0 > /sys/fs/selinux/enforce
 setfiles -r /mnt -- "/etc/selinux/$SELINUXTYPE/contexts/files/file_contexts" /mnt
