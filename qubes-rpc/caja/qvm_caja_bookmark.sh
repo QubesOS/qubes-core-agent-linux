7aab8e09d480de71ad4c0ea2a1a0acdee121cdae
Demi Marie Obenour <demi@invisiblethingslab.com>
2023-04-20 16:29:57 -0400

    Remove qvm_caja_bookmark.sh
    It hardcoded /home/user and was never called.

--- qubes-rpc/caja/qvm_caja_bookmark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ ! -e ~/.config/gtk-3.0/qubes-incoming-bookmark-created ]
-then
-  echo "file:///home/user/QubesIncoming" >> ~/.config/gtk-3.0/bookmarks
-  touch ~/.config/gtk-3.0/qubes-incoming-bookmark-created
-fi
