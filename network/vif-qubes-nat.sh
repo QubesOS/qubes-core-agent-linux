41fff23c6dae16e6eb3f686a79297f1fc5cd8ca7
Demi Marie Obenour <demi@invisiblethingslab.com>
2023-04-21 16:09:20 -0400

    Add permanent neighbor entries again
    Previously, neighbor discovery used ARP or NDP, both of which have
    nonzero attack surface.  Additionally, the strict anti-spoofing rules
    used by Qubes OS tend to break NDP, causing IPv6 traffic to be
    unreliable.
    
    This solves the second problem by adding permanent entries to Linux's
    neighbor cache.  Since the peer MAC address is already known, it is not
    necessary to use ARP or NDP to discover it.  This allows NDP to be
    blocked outright if it has not been blocked already.

--- network/vif-qubes-nat.sh
+++ network/vif-qubes-nat.sh
@@ -86,6 +86,8 @@ table netdev antispoof {
     }
 }"
 
+    netns ip neighbour add to "$appvm_ip" dev "$netns_appvm_if" lladdr "$mac" nud permanent
+    netns ip neighbour add to "$netvm_gw_ip" dev "$netns_netvm_if" lladdr "$netvm_mac" nud permanent
     netns ip addr add "$netvm_ip" dev "$netns_netvm_if"
     netns ip addr add "$appvm_gw_ip" dev "$netns_appvm_if"
 
