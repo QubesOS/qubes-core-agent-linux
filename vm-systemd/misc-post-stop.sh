1f10b4de4b7984bb936936f26c2bd25b8cb6e15b
kennethrrosen <kennethrrosen@proton.me>
2024-07-22 10:16:08 +0000

    Misc-post-stop: remove unused QubesIncoming
    reaolves https://github.com/QubesOS/qubes-issues/issues/8307

--- vm-systemd/misc-post-stop.sh
+++ vm-systemd/misc-post-stop.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-# Save default applications for DispVM
+# cleanup QubesIncoming/
+if [ -d /home/user/QubesIncomming ]; then
+    find /home/user/QubesIncoming -mindepth 1 -type f -empty -delete
+    find /home/user/QubesIncoming -mindepth 1 -type d -empty -delete
+fi
 
+# Save default applications for DispVM
 exit 0
