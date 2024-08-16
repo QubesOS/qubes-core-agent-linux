b2dc6229bf2375b573e72f087913b08a7c10aab6
Ali Mirjamali <ali@mirjamali.com>
2024-08-16 13:34:31 +0330

    Allow recent:/// in Nautilus for Copy/Move/Open
    fixes: https://github.com/QubesOS/qubes-issues/issues/8589

--- qubes-rpc/nautilus/qvm_copy_nautilus.py
+++ qubes-rpc/nautilus/qvm_copy_nautilus.py
@@ -28,10 +28,30 @@ class CopyToAppvmItemExtension(GObject.GObject, Nautilus.MenuProvider):
     def on_menu_item_clicked(self, menu, files):
         '''Called when user chooses files though Nautilus context menu.
         '''
-        cmd = [file_obj.get_location().get_path()
-               for file_obj in files
-               # Check if file is not gone
-               if not file_obj.is_gone()]
+        paths = []
+        for file_obj in files:
+            file_location = file_obj.get_location()
+            file_uri = file_location.get_uri()
+            if file_uri.startswith('file:///'):
+                if not file_obj.is_gone():
+                    # Check if file is not gone
+                    paths.append(file_location.get_path())
+            elif file_uri.startswith('recent:///'):
+                try:
+                    file_info = file_location.query_info(
+                            Gio.FILE_ATTRIBUTE_STANDARD_TARGET_URI, 0, None)
+                    target_uri = file_info.get_attribute_string(
+                            Gio.FILE_ATTRIBUTE_STANDARD_TARGET_URI)
+                    if target_uri.startswith('file://'):
+                        paths.append(target_uri[7:])
+                except GLib.GError:
+                    # TODO: Decide what to do if the recent item does not exist
+                    pass
+            else:
+                # TODO: Decide what to do with other weird URIs (eg. smb:///)
+                pass
+        # Double-check if the file is not gone in the meantime
+        cmd = [path for path in paths if os.path.exists(path)]
         cmd.insert(0, '/usr/lib/qubes/qvm-copy-to-vm.gnome')
         pid = GLib.spawn_async(cmd)[0]
         GLib.spawn_close_pid(pid)
