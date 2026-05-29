de1f091b376579c3cfa4ebeb8d7cef072980d432
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2026-05-29 03:33:16 +0200

    Fix issues found by shellcheck
    Specifically: SC2268 (style): Avoid x-prefix in comparisons as it no longer serves a purpose.
    (cherry picked from commit 8a74a7db7b8f83e13b31055cafa8a2f0cc7deba9)

--- vm-systemd/network-uplink-wait.sh
+++ vm-systemd/network-uplink-wait.sh
@@ -7,7 +7,7 @@
 # Setup IP address at specific time of system boot, instead of asynchronously
 # by udev
 QUBES_MANAGED_IFACE="$(get_qubes_managed_iface)"
-if [ "x$QUBES_MANAGED_IFACE" != "x" ]; then
+if [ -n "$QUBES_MANAGED_IFACE" ]; then
     # systemd does not support conditional After= dependencies, nor a tool to
     # just wait for the unit to be activated
     # if the network interface is expected, use `systemctl start` to wait for
